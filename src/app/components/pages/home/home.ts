a5bc5a7dc9adc8523b0adb2e56e9f4f06d7e150e
Ismael Monleón Rubio <ismael.monleon73@gmail.com>
2026-01-21 12:26:28 +0100

    a

---
+++ src/app/components/pages/home/home.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  imports: [RouterLink],
+  templateUrl: './home.html',
+  styleUrl: './home.scss',
+})
+export class Home {
+  activeCard: string | null = null;
+
+  setActive(card: string) {
+    this.activeCard = this.activeCard === card ? null : card;
+  }
+}
